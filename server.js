1fc4a17feeb66c8c551b4e23b5f1ac5eccdcabab
John Mohlenkamp <john@mohlenkamp.com>
2021-06-26 10:05:59 -0500

    removed express from solution b/c we're not using it... Tired

--- server.js
+++ server.js
@@ -3,29 +3,10 @@ const inquirer = require('inquirer'); //Inquirer
 const db = require('./db/connection');  
 const cTable = require('console.table');
 const questions = require('./utils/Questions')
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
 
-// Express middleware requirements
-const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-//app.use(express.static('public')); -- Not needed b/c this is command line
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
 
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
 });
